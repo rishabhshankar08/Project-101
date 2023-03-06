3becfc00d6541f44f2de3d01488313fe2584d0fc
rishabhshankar08 <82026135+rishabhshankar08@users.noreply.github.com>
2023-03-06 19:07:06 +0530

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,58 @@
+import  random
+value = 0
+response = input("Enter y to roll a dice and n to exit : ")
+if(response == 'y'):
+    value += 1
+
+
+
+while response == 'y':
+    dice_no = random.randint(1,6)
+    if(dice_no == 1):
+        print("[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("[-----]")
+        response = input("Enter y to roll a dice and n to exit : ")
+    elif(dice_no == 2):
+        print("[-----]")
+        print("[     ]")
+        print("[ 0 0 ]")
+        print("[     ]")
+        print("[-----]")
+        response = input("Enter y to roll a dice and n to exit : ")
+    elif(dice_no == 3):
+        print("[-----]")
+        print("[     ]")
+        print("[0 0 0]")
+        print("[     ]")
+        print("[-----]")
+        response = input("Enter y to roll a dice and n to exit : ")
+    elif(dice_no == 1):
+        print("[-----]")
+        print("[0   0]")
+        print("[     ]")
+        print("[0   0]")
+        print("[-----]")
+        response = input("Enter y to roll a dice and n to exit : ")
+    elif(dice_no == 1):
+        print("[-----]")
+        print("[0   0]")
+        print("[  0  ]")
+        print("[0   0]")
+        print("[-----]")
+        response = input("Enter y to roll a dice and n to exit : ")
+    else:
+        print("[-----]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[-----]")
+        response = input("Enter y to roll a dice and n to exit : ")
+
+if(response == 'n'):
+     if(value == 1):
+      print("Thanks for rolling!")
+     else:
+        print("Roll next time")
